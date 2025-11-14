e567c3791726c190ca9fb2d772e497ef40a86dc8
ken <osoriokenneth91@gmail.com>
2025-11-14 12:37:37 +0800

    Updated app.py with all imports across the db files

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+from config import Config
+from models.db import db
+from models.user_model import User
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route("/")
 def login():
@@ -11,5 +23,6 @@ def register():
     return render_template('register.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = os.getenv('PORT')
+    app.run(debug=True, port=port)
 
