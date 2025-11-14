f491cd214b5a2ae4962d80399f1852596f101ee4
ken <osoriokenneth91@gmail.com>
2025-11-14 14:34:37 +0800

    Revert "Merge pull request #30 from kennethosorio91/huerto-creation-of-create_db.py-7"
    This reverts commit 06f44336cf500c97fb37d67fb116bc3ba371824e, reversing
    changes made to 2f5fc12215417494e8f9e48e2e63e5870865cf7b.

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
 from flask import Flask, render_template
-from config import Config
-from models.db import db
-from models.user_model import user
-
-import os
-
-db.init_app(app)
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
 @app.route("/")
 def login():
@@ -18,7 +10,6 @@ def login():
 def register():
     return render_template('register.html')
 
-if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5055))
-    app.run(debug=True, port=port)
+if __name__ == "__main__":
+    app.run(debug=True)
 
